b7f42c1cb6f9da713d6bc615db4d8ee35457ddea
gauravgangwarweb <gauravgangwar501@gmail.com>
2023-02-09 18:49:18 +0530

    Add test for reducer file

---
+++ src/Tests/reducer.test.js
@@ -0,0 +1,36 @@
+import coinsSlice, { selectCoin } from '../Redux/Reducer/coinReducer';
+
+const initialState = {
+  data: [],
+  selectedCoin: [],
+  loading: false,
+  error: null,
+};
+
+describe('test redux', () => {
+  it('if it is eth', () => {
+    expect(coinsSlice(initialState, selectCoin(['eth']))).toEqual({
+      data: [],
+      selectedCoin: ['eth'],
+      loading: false,
+      error: null,
+    });
+  });
+
+  it('if it is btc', () => {
+    expect(coinsSlice(initialState, selectCoin(['btc']))).toEqual({
+      data: [],
+      selectedCoin: ['btc'],
+      loading: false,
+      error: null,
+    });
+  });
+  it('if it is sol', () => {
+    expect(coinsSlice(initialState, selectCoin(['sol']))).toEqual({
+      data: [],
+      selectedCoin: ['sol'],
+      loading: false,
+      error: null,
+    });
+  });
+});
