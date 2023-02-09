dfd348b5266139d99da468c69430749450eea42f
gauravgangwarweb <gauravgangwar501@gmail.com>
2023-02-09 19:30:01 +0530

    Fix linter errors

--- src/Tests/details.test.js
+++ src/Tests/details.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from '../Redux/ConfigureStore';
+import store from '../Redux/ConfigureStore.js';
 import Details from '../Components/Details';
 import '@testing-library/jest-dom/extend-expect';
 
